933a6435a4b14dd41ca208ef37b37fd0868bf5b1
EmyliePM <emylieperiot@gmail.com>
2026-08-07 15:05:53 +0100

    changed the experience or 'enter' page to be more in theme with space and the big bang for high impact and experience

--- components/home/Experience.tsx
+++ components/home/Experience.tsx
@@ -1,84 +1,58 @@
-import Image from "next/image";
+import styles from "./Experience.module.css";
 import Link from "next/link";
 
-import styles from "./Experience.module.css";
-
-const projects = [
-  {
-    title: "Craftworkz",
-    url: "craftworkz.co.uk",
-    image: "/images/sites/craftworkz.jpg",
-    href: "#",
-    colourClass: styles.blue,
-    positionClass: styles.projectOne,
-  },
-  {
-    title: "Periot Reworked",
-    url: "periotreworked.co.uk",
-    image: "/images/sites/reworked.jpg",
-    href: "#",
-    colourClass: styles.purple,
-    positionClass: styles.projectTwo,
-  },
-  {
-    title: "Periot Digital",
-    url: "periotdigital.co.uk",
-    image: "/images/sites/periot-digital.jpg",
-    href: "/",
-    colourClass: styles.pink,
-    positionClass: styles.projectThree,
-  },
-  {
-    title: "Concept Project",
-    url: "concept-project.co.uk",
-    image: "/images/sites/concept.jpg",
-    href: "#",
-    colourClass: styles.green,
-    positionClass: styles.projectFour,
-  },
-];
 
 export default function Experience() {
   return (
     <main className={styles.experience}>
-      <div className={styles.particles} aria-hidden="true" />
-
-      <Link href="/" className={styles.back}>
-        <span aria-hidden="true">←</span>
-        Exit
-      </Link>
-
-      <div className={styles.stage}>
-        {projects.map((project) => (
-          <Link
-            key={project.title}
-            href={project.href}
-            aria-label={`View ${project.title}`}
-            className={`${styles.project} ${project.positionClass}`}
-          >
-            <div className={`${styles.frame} ${project.colourClass}`}>
-              <p className={styles.url}>
-                <span aria-hidden="true">↗</span>
-                {project.url}
-              </p>
-
-              <div className={styles.window}>
-                <div className={styles.imageWrapper}>
-                  <Image
-                    src={project.image}
-                    alt={`${project.title} website preview`}
-                    fill
-                    sizes="(max-width: 800px) 75vw, 35vw"
-                    className={styles.image}
-                  />
-                </div>
-              </div>
-            </div>
-          </Link>
-        ))}
+      {/* Deep space */}
+      <div className={styles.background}>
+        <div className={styles.stars} aria-hidden="true" />
+        <div className={styles.nebula} aria-hidden="true" />
       </div>
 
-      <p className={styles.instruction}>Move through the work</p>
+      {/* Boot sequence */}
+      <section className={styles.boot}>
+        <div className={styles.bootMessages}>
+          <p className={styles.bootText}>
+            INITIALISING
+            <span className={styles.loading}>
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </span>
+          </p>
+
+          <p className={styles.bootText}>
+            SYNCHRONISING ASSETS
+            <span className={styles.loading}>
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </span>
+          </p>
+
+          <p className={styles.connected}>
+            &gt; LINK ESTABLISHED
+          </p>
+        </div>
+      </section>
+
+      {/* Digital pulse */}
+      <div className={styles.pulse} aria-hidden="true">
+        <div className={styles.ring} />
+      </div>
+<Link href="/" className={styles.exit}>
+  <span aria-hidden="true">◄</span>
+  <span>EXIT</span>
+</Link>
+
+      {/* Workspace */}
+      <section className={styles.workspace}>
+        {/* Digital debris */}
+        {/* Projects */}
+        {/* Open Studio portal */}
+      </section>
     </main>
   );
 }
